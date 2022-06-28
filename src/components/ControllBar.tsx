f467860d7634589b2318693cb1b37058ebfb4efa
Daniel Karlsson <danier1999@gmail.com>
2022-06-28 09:29:22 +0200

    ControllBar done

--- src/components/ControllBar.tsx
+++ src/components/ControllBar.tsx
@@ -13,13 +13,14 @@ import { setCounter } from '../context/slices/gameDataSlice';
 
 function ControllBar(){
 
-const frameCount = useSelector((state: RootState) => state.gameData.messages.length);
-const currentFrame = useSelector((state: RootState) => state.gameData.counter);
-const dispatch = useDispatch();
+  const frameCount = useSelector((state: RootState) => state.gameData.messages.length);
+  const currentFrame = useSelector((state: RootState) => state.gameData.counter);
+  const dispatch = useDispatch();
 
   const [running, setRunning] = useState(false);
   const [frequency, setFrequency] = useState(constants.STARTING_FREQUENCY);
   let intervalID: NodeJS.Timer;
+  const gameEnded = () => currentFrame >= frameCount-1;
 
   useEffect(() => {
     if (running) {
@@ -30,51 +31,67 @@ const dispatch = useDispatch();
       }, frequency);
     }
     return () => clearInterval(intervalID);
-
   }, [running, frequency]);
 
-    return (
-        <div className="box controlpanel">
+  useEffect(() => {
+    if (gameEnded()) {
+      setRunning(false);
+    }
+  }, [gameEnded]);
+
+  function getPlayIcon() {
+    if (gameEnded()) {
+      return Replay;
+    }
+    if (!running) {
+      return Play;
+    }
+    return Pause;
+  }
+
+  return (
+      <div className="box controlpanel">
+      <input
+        type="range"
+        step="1"
+        min="3"
+        max={frameCount - 1}
+        value={currentFrame}
+        className="react-native-slider"
+        onChange={(e) => {
+          console.log("event value:", e.target.value);
+          dispatch(setCounter(parseInt(e.target.value)));
+          messageDispatch(false);
+        }}
+      />
+      <div className="controlbuttons">
         <input
-          type="range"
-          step="1"
-          min="3"
-          max={frameCount}
-          value={currentFrame}
-          className="react-native-slider"
-          onChange={(e) => {
-            console.log("event value:", e.target.value);
-            dispatch(setCounter(parseInt(e.target.value)));
-            messageDispatch(false);
+          type="image"
+          src={Backwards}
+          name="ButtonBackwards"
+          className="backwardsButton"
+          onClick={() => setFrequency(frequency + constants.FREQUENCY_STEP)}
+        />
+        <input
+          type="image"
+          src={getPlayIcon()} 
+          name="PlayButton"
+          className="playButton"
+          onClick={()=>{
+            setRunning(!running);
+            if (gameEnded()) dispatch(setCounter(3)); // 3 is the first frame with snake positions
           }}
         />
-        <div className="controlbuttons">
-          <input
-            type="image"
-            src={Backwards}
-            name="ButtonBackwards"
-            className="backwardsButton"
-            onClick={() => setFrequency(frequency + constants.FREQUENCY_STEP)}
-          />
-          <input
-            type="image"
-            src={running ? Pause : Play} 
-            name="PlayButton"
-            className="playButton"
-            onClick={()=>{
-              setRunning(!running);
-            }}
-          />
-          <input
-            type="image"
-            src={Forwards}
-            name="ButtonForward"
-            className="forwardButton"
-            onClick={()=> setFrequency(Math.max(frequency - constants.FREQUENCY_STEP, constants.MIN_FREQUENCY))}
-          />
-        </div>
+        <input
+          type="image"
+          src={Forwards}
+          name="ButtonForward"
+          className="forwardButton"
+          onClick={()=> setFrequency(Math.max(frequency - constants.FREQUENCY_STEP, constants.MIN_FREQUENCY))}
+        />
       </div>
-    );
+    </div>
+  );
 }
 
 export default ControllBar;
