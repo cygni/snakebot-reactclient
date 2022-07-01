b3e691bf628d9e2b6f33b093ede69c763f790b1a
Daniel Karlsson <danier1999@gmail.com>
2022-07-01 14:21:36 +0200

    Scoreboard sort by alive, points, name
    Co-authored-by: SebastianH-ctrl <SebastianH-ctrl@users.noreply.github.com>
    Co-authored-by: Olivia <oliviaharlin@users.noreply.github.com>

--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -5,11 +5,38 @@ import type { RootState } from '../context/store';
 function ScoreBoard() {
   const snakes = useSelector((state: RootState) => state.snakes.snakesData);
 
+  //SORT BY 1. ALIVE, 2. POINTS, 3.NAME IN THAT ORDER
+  function sortSnakes(snakeID_one: string, snakeID_two: string){
+    const snakeOne = snakes[snakeID_one];
+    const snakeTwo = snakes[snakeID_two];
+
+    
+    if (!snakeOne.alive && snakeTwo.alive) {
+      return 1;
+    } else if (!snakeTwo.alive && snakeOne.alive) {
+      return -1;
+    }
+  
+    const pointDiff = snakeTwo.points - snakeOne.points;
+    if (pointDiff !== 0) {
+      return pointDiff;
+    }
+  
+    if (snakeOne.name > snakeTwo.name) {
+      return 1;
+    } else if (snakeTwo.name < snakeTwo.name) {
+      return -1;
+    }
+  
+    return 0;
+
+  }
+
   return (
     <div className="box activePlayers">
         <ul>
             {
-            Object.keys(snakes).sort((aID, bID) => snakes[bID].points - snakes[aID].points).map((snakeID, index: number) => (
+            Object.keys(snakes).sort(sortSnakes).map((snakeID, index: number) => (
             <li key={index}>
               <>
                 <figure>
