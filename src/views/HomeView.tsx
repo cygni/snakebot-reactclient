6247f65e27face452de5ffca44d5fd3046acb48d
Daniel Karlsson <danier1999@gmail.com>
2022-07-18 11:35:50 +0200

    Temp commit to test if latest image is pulled in prod

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -7,7 +7,7 @@ function HomeView() {
         <h1>Welcome!</h1>
         <div className="text-content">
           <p>
-            Remember the old game of Snake? One of the first common
+            WOW THIS IS A TEST TO SEE IF IT HAS LATEST IMAGE!. Remember the old game of Snake? One of the first common
             implementations was available on the phone Nokia 3310.<br />
             <a
               href="https://www.youtube.com/watch?v=wjEdCd4t1m8"
